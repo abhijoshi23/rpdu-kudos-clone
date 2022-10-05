a7e41f9e3260599530c2cd03da5e120163230b06
Siranchao <scao34@myseneca.ca>
2022-10-05 17:04:28 -0400

    add node modules

--- kudos-app/gatsby-config.js
+++ kudos-app/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kudos`,
+    description: `Welcome to Kudos App - Designed and developed by RPDU`,
+    author: `RPDU`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
